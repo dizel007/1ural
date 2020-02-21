71f94014a5b37543d8a6ac834231dbb43f89c4e3
Dmitriy <dizel007@yandex.ru>
2020-02-21 17:28:38 +0500

    начал делать каталог на JS

--- dist/js/catalog.js
+++ dist/js/catalog.js
@@ -1,5 +1,6 @@
 
-function myFunction() {
-   var element = document.getElementById("lineDrain");
-   element.classList.toggle("mystyle");
-}
+$('ul.advantages__items').on('click', 'li:not(.advantages__item_active)', function() {
+    $(this)
+      .addClass('advantages__item_active').siblings().removeClass('advantages__item_active');
+    //   .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+});
